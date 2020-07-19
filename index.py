82b4452123729a6cdaa0f0c4a158cc2bb6c6705b
rishitaggarwal1 <rishitaggarwal1@gmail.com>
2020-07-19 18:35:02 +0530

    Removing Stopwords and Tokenization

--- index.py
+++ index.py
@@ -32,3 +32,29 @@ def form_sentence(tweet):
 
 print(form_sentence(train_tweets['tweet'].iloc[10]))
 print(train_tweets['tweet'].iloc[10])
+
+
+def no_user_alpha(tweet):
+    tweet_list = [ele for ele in tweet.split() if ele != 'user']
+    clean_tokens = [t for t in tweet_list if re.match(r'[^\W\d]*$', t)]
+    clean_s = ' '.join(clean_tokens)
+    clean_mess = [word for word in clean_s.split() if word.lower()
+                  not in stopwords.words('english')]
+    return clean_mess
+
+
+print(no_user_alpha(form_sentence(train_tweets['tweet'].iloc[10])))
+print(train_tweets['tweet'].iloc[10])
+
+
+def normalization(tweet_list):
+    lem = WordNetLemmatizer()
+    normalized_tweet = []
+    for word in tweet_list:
+        normalized_text = lem.lemmatize(word, 'v')
+        normalized_tweet.append(normalized_text)
+    return normalized_tweet
+
+
+tweet_list = 'I was playing with my friends with whom I used to play, when you called me yesterday'.split()
+print(normalization(tweet_list))
