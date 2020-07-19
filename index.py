e67262d58e67ed94c2840d96a28d845788c969c2
rishitaggarwal1 <rishitaggarwal1@gmail.com>
2020-07-19 18:31:13 +0530

    Changes Made

--- index.py
+++ index.py
@@ -1,8 +1,20 @@
 # Importing Libraries
-import tweepy
-from textblob import TextBlob
-from wordcloud import WordCloud
+#Data Analysis
 import pandas as pd
-import numpy as np
-import re
 import matplotlib.pyplot as plt
+import seaborn as sns
+
+#Data Preprocessing and Feature Engineering
+from textblob import TextBlob
+import re
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+
+#Model Selection and Validation
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import confusion_matrix, classification_report,accuracy_score
+
+
